170fe5884fecf7fdc7adbf3fff08855fd70aecef
Alex Chan <135670641+Glesyni@users.noreply.github.com>
2024-09-11 13:17:00 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        final String name = "Alex";
+        int age = 17; 
+        double age_months = 3; 
+        boolean is_adult = false;
+        double total_age = age + (age_months/12); 
+        System.out.println(total_age);
+    }
+}
