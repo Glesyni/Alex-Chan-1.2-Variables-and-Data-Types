63b34b2ff93243c01f0e5a81f70e631425eec1c6
Alex Chan <135670641+Glesyni@users.noreply.github.com>
2024-09-11 13:21:11 +0000

    Final Version

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ public class Main
         double age_months = 3; 
         boolean is_adult = false;
         double total_age = age + (age_months/12); 
-        System.out.println(total_age);
+        System.out.println("name: " + name);
+        System.out.println("Age: " + total_age);
+        System.out.println("Is an Adult: " + is_adult); 
     }
 }
